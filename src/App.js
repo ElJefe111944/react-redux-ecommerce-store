6cba06b7263a7f32bb15e4e9e2425f1ce92cbedd
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2022-10-25 08:47:37 +0100

    Product Actions, Action types and reducers created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './containers/Header'
+import ProductListing from './containers/ProductListing'
+import ProductDetails from './containers/ProductDetails'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
