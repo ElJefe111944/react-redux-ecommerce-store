85799036564dcb8ae6c552d960470edafc7ffcee
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2022-10-26 08:06:37 +0100

    API fetch functionality added to retrieve the data for all products

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 import { useSelector } from 'react-redux'
 import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
-  console.log(products)
+
+    const fetchProducts = async () => {
+      const response = await axios.get('https://fakestoreapi.com/products')
+      .catch((err) => {
+        console.log('Error: ', err);
+      })
+      console.log(response.data)
+    };
+
+    useEffect(() => {
+      fetchProducts();
+    }, [])
 
   return (
     <div className='ui grid container'>
