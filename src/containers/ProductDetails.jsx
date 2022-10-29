05445ec83e0eed8bee05623f0e7a43014c68aac1
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2022-10-29 14:38:37 +0100

    Final styling added to PLP & PDP

--- src/containers/ProductDetails.jsx
+++ src/containers/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedProduct, removeSelectedProduct } from '../redux/actions/ProductActions';
+import { Link } from 'react-router-dom';
 import './productDetailStyles.css'
 
 const ProductDetails = () => {
@@ -28,40 +29,42 @@ const ProductDetails = () => {
     };
   }, [productId]);
   return (
-    <div>
-      <div className="ui grid container">
-        {Object.keys(product).length === 0 ? (<div>...Loading</div>) : (
-          <div className="ui placeholder segment">
-            <div className="ui two colum stackable center aligned grid">
-              <div className="ui vertical divider"> AND</div>
-              <div className="middle aligned row">
-                <div className="column lp">
-                  <img src={image} alt="/" className="ui fluid image" />
-                </div>
-                <div className="column rp">
-                  <h1>{title}</h1>
-                  <h2>
-                    <a href="" className="ui teal tag label">£{price}</a>
-                  </h2>
-                  <h3 className="ui brown block header">{category}</h3>
-                  <p>{description}</p>
-                  <div className="ui vertical animated button" tabIndex='0'>
-                    <div className="hidden content">
-                      <i className="shop icon"></i>
-                    </div>
-                    <div className="visible content">
-                      Add to Cart
-                    </div>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
                   </div>
+                  <div className="visible content">Add to Cart</div>
                 </div>
               </div>
             </div>
           </div>
-        )}
-
+        </div>
+      )}
+      <div className="btn-container">
+        <Link to={`/`}>
+          <button>Return</button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetails
